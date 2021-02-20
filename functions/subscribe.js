c8bfd61ccfe43bd9d4befc1f0bb45214e4cc38d5
Jeff Carouth <jcarouth@gmail.com>
2021-02-19 20:56:24 -0600

    Stub out function call from form submit

--- functions/subscribe.js
+++ functions/subscribe.js
@@ -1,9 +1,48 @@
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+}
+
 exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      status: 'subscribed',
-      email: '',
-    })
+
+  const { email, listId } = JSON.parse(event.body)
+
+  if (!email || !listId) {
+    return {
+      statusCode: 400,
+      headers,
+      body: JSON.stringify({
+        status: 'error',
+        error: 'Missing data: email and listId are required',
+      })
+    }
+  }
+
+  if (email === 'success@email.com') {
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({
+        status: 'pending',
+      })
+    }
+  } else if (email === 'alreadysubscribed@email.com') {
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({
+        status: 'subscribed',
+      })
+    }
+  } else {
+    return {
+      statusCode: 400,
+      headers,
+      body: JSON.stringify({
+        status: 'error',
+        error: 'Mixed',
+      })
+    }
   }
 }
--- source/_assets/js/components/newsletter.js
+++ source/_assets/js/components/newsletter.js
@@ -1,7 +1,13 @@
 window.newsletter = function() {
   return {
+    config: {
+      endpoints: {
+        subscribe: '/.netlify/functions/subscribe',
+      },
+    },
     data: {
       email: '',
+      listId: '1234',
     },
     error: false,
     loading: false,
@@ -30,15 +36,29 @@ window.newsletter = function() {
         return false;
       }
 
-      if (this.data.email === 'success@email.com') {
-        this.setSuccess('You have been subscribed. Please check your email to confirm.');
-      } else if (this.data.email === 'alreadysubscribed@email.com') {
-        this.setSuccess('You are already subscribed. Thank you!');
-      } else {
-        this.setError('We could not subscribe you. Please try again.');
-      }
+      let self = this;
 
-      this.reset();
+      fetch(this.config.endpoints.subscribe, {
+        method: 'POST',
+        body: JSON.stringify(this.data),
+        headers: { 'Content-Type': 'application/json' }
+      })
+        .then(r => r.json())
+        .then(data => {
+          if (data.status === 'pending') {
+            self.setSuccess('You have been subscribed. Please check your email to confirm.');
+          } else if (data.status === 'subscribed') {
+            self.setSuccess('You are already subscribed. Thank you!');
+          } else {
+            self.setError('We could not subscribe you. Please try again.');
+          }
+        })
+        .catch(e => {
+          self.setError('We could not subscribe you. Please try again.');
+        })
+        .finally(() => {
+          self.reset();
+        });
     },
   };
 };
