0919d433b7043f06ae41a57656c19f8a760bcb66
Jeff Carouth <jcarouth@gmail.com>
2021-02-19 19:11:00 -0600

    Stub out behavior of success/error states

--- source/_assets/js/components/newsletter.js
+++ source/_assets/js/components/newsletter.js
@@ -8,11 +8,37 @@ window.newsletter = function() {
     message: '',
     reset() {
       this.loading = false;
-      this.data.email = '';
+      if (!this.error) {
+        this.data.email = '';
+      }
+    },
+    setError(message) {
+      this.error = true;
+      this.message = message;
+    },
+    setSuccess(message) {
+      this.error = false;
+      this.message = message;
     },
     submit() {
-      this.loading = !this.loading;
-      this.message = 'You have been subscribed. Please check your email to confirm.'
+      this.loading = true;
+
+      if (this.data.email === '') {
+        this.setError('Please enter your email address.');
+        this.loading = false;
+
+        return false;
+      }
+
+      if (this.data.email === 'success@email.com') {
+        this.setSuccess('You have been subscribed. Please check your email to confirm.');
+      } else if (this.data.email === 'alreadysubscribed@email.com') {
+        this.setSuccess('You are already subscribed. Thank you!');
+      } else {
+        this.setError('We could not subscribe you. Please try again.');
+      }
+
+      this.reset();
     },
   };
 };
